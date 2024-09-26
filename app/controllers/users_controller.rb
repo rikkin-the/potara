6f60ebb5d28b86af34a6cadb30d00671d842d6fc
riku_nakama <k74rikkin@outlook.com>
2024-09-26 19:17:14 +0900

    Complete basic user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
+      flash[:success] = "Muberへようこそ！"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#base"
   get "/base", to: "home#base"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
